1d987f5fb3423d9e242d9a9a85402dec7ce61ffe
RishalX <xavierrishal@gmail.com>
2026-02-26 14:25:31 +0530

    UC1: Application Entry & Welcome Message

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,5 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("welcome to the palindrome checker app\n"+ "version:1.0\n" + "system initialized successfully.\n");
+    }
+}
