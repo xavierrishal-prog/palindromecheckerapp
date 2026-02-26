147b26b8012813da37416fa92082120df55aee5b
RishalX <xavierrishal@gmail.com>
2026-02-26 16:00:07 +0530

    uc4-array based palindrome check

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,5 +1,19 @@
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("welcome to the palindrome checker app\n"+ "version:1.0\n" + "system initialized successfully.\n");
+    public static void main(String[] args){
+        String name="radar";
+        char[] chars = name.toCharArray();
+        int start=0;
+        int end= chars.length-1;
+        boolean isPalindrome=true;
+        while(start<end){
+            if(chars[start] != chars[end]){
+                isPalindrome=false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("input text : "+name);
+        System.out.print("Is it a Palindrome? : "+isPalindrome);
     }
 }
