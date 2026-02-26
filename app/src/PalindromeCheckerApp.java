d00cdac5b163e031cf9a3f1d858473a78e3c63f4
RishalX <xavierrishal@gmail.com>
2026-02-26 15:45:18 +0530

    uc3-using string reverse

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,5 +1,29 @@
+import  java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("welcome to the palindrome checker app\n"+ "version:1.0\n" + "system initialized successfully.\n");
+    public static  void main(String[] args){
+        System.out.println("input:");
+        Scanner scanner=new Scanner(System.in);
+        String original =(scanner.next());
+        String Reverse ="";
+        boolean present=false;
+        for (int i=original.length()-1;i>=0;i--){
+            Reverse=Reverse+original.charAt(i);
+        }
+        for (int i=0;i<original.length();i++){
+            if(original.charAt(i)==Reverse.charAt(i)){
+                present=true;
+            }else {
+                present=false;
+            }
+        }
+        if(present==true){
+            System.out.println(original+" and "+Reverse+" are Palindrome");
+        }else{
+            System.out.println(original+" and "+Reverse+" are not  Palindrome");
+        }
+
+
+
     }
 }
