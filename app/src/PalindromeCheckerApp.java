09f5fe9994a5d10142da6916ccb6d550e64f6b39
RishalX <xavierrishal@gmail.com>
2026-02-26 16:09:26 +0530

    uc5-stack based palindrome

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,19 +1,21 @@
+import javax.xml.namespace.QName;
+import java.util.Stack;
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        String name="radar";
-        char[] chars = name.toCharArray();
-        int start=0;
-        int end= chars.length-1;
-        boolean isPalindrome=true;
-        while(start<end){
-            if(chars[start] != chars[end]){
-                isPalindrome=false;
+    public static void main(String[] args) {
+        String name = "noon";
+        Stack<Character> stack = new Stack<>();
+        for (char c : name.toCharArray()) {
+            stack.push(c);
+        }
+        boolean ispalindrome = true;
+        for (char c : name.toCharArray()) {
+            if (c != stack.pop()) {
+                ispalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
-        System.out.println("input text : "+name);
-        System.out.print("Is it a Palindrome? : "+isPalindrome);
+
+        System.out.println("input text : " + name);
+        System.out.print("Is it a Palindrome? : " + ispalindrome);
     }
 }
